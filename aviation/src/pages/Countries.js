adbd4bddc7b418d72d4bdfe12b3ff2a7f871d9fd
SeppeK-1952233 <seppe.kimps@student.uhasselt.be>
2025-04-19 16:30:31 +0200

    Legenda countries map

--- aviation/src/pages/Countries.js
+++ aviation/src/pages/Countries.js
@@ -85,6 +85,70 @@ const Countries = () => {
         g.attr("transform", event.transform);
       });
     svg.call(zoom);
+
+    // Clean old legend
+    d3.select("#legend").selectAll("*").remove();
+
+    const legendWidth = 300;
+    const legendHeight = 10;
+    const margin = { top: 0, right: 20, bottom: 20, left: 20 };
+
+    const svgLegend = d3
+      .select("#legend")
+      .append("svg")
+      .attr("width", legendWidth + margin.left + margin.right)
+      .attr("height", legendHeight + margin.bottom);
+
+    const defs = svgLegend.append("defs");
+
+    const gradient = defs
+      .append("linearGradient")
+      .attr("id", "legend-gradient")
+      .attr("x1", "0%")
+      .attr("x2", "100%");
+
+    gradient
+      .append("stop")
+      .attr("offset", "0%")
+      .attr("stop-color", "#f2f0f7"); // Light color
+
+    gradient
+      .append("stop")
+      .attr("offset", "100%")
+      .attr("stop-color", "#8b0000"); // Dark red
+
+    svgLegend
+      .append("rect")
+      .attr("x", margin.left)
+      .attr("y", 0)
+      .attr("width", legendWidth)
+      .attr("height", legendHeight)
+      .style("fill", "url(#legend-gradient)");
+
+    const minVal = d3.min(data, (d) => d.Count);
+    const maxVal = d3.max(data, (d) => d.Count);
+
+    const xScale = d3
+      .scaleLinear()
+      .domain([minVal, maxVal])
+      .range([0, legendWidth]);
+
+    const tickValues = d3.range(0, 6).map((i) =>
+      Math.round(minVal + (i * (maxVal - minVal)) / 5)
+    );
+
+    const xAxis = d3
+      .axisBottom(xScale)
+      .tickValues(tickValues)
+      .tickFormat(d3.format("~s"));
+
+    svgLegend
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${legendHeight})`)
+      .call(xAxis)
+      .selectAll("text")
+      .style("font-size", "10px");
+
   }, [data]);
 
   if (error) {
@@ -95,12 +159,17 @@ const Countries = () => {
     <div className="h-screen flex">
       <SidePanel />
 
-      <div className="flex-1 bg-white p-4 overflow-auto">
+      <div className="relative w-full h-full border">
         <svg
           id="map"
           viewBox="0 0 960 600"
-          className="w-full h-full border"
+          className="w-full h-full"
         ></svg>
+
+        <div
+          id="legend"
+          className="absolute bottom-2 right-2 bg-white/80 p-2 rounded shadow"
+        ></div>
       </div>
 
       <CountriesFilter onDateChange={handleFilter} />
